d2017d452f8e78a433f9b44d8994be55a13fcc2c
David Benjamin <davidben@broadinstitute.org>
2019-01-29 17:23:29 -0500

    work

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/SomaticPriorModel.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/mutect/filtering/SomaticPriorModel.java
@@ -2,19 +2,26 @@ package org.broadinstitute.hellbender.tools.walkers.mutect.filtering;
 
 import htsjdk.variant.variantcontext.VariantContext;
 import org.apache.commons.lang3.mutable.MutableDouble;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.bouncycastle.crypto.prng.RandomGenerator;
 import org.broadinstitute.hellbender.tools.walkers.mutect.Mutect2Engine;
 import org.broadinstitute.hellbender.tools.walkers.mutect.MutectStats;
+import org.broadinstitute.hellbender.tools.walkers.readorientation.BetaDistributionShape;
 import org.broadinstitute.hellbender.utils.MathUtils;
+import org.broadinstitute.hellbender.utils.Utils;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalDouble;
+import java.util.*;
 
 public class SomaticPriorModel {
     private double log10SNVPrior;
     private double log10IndelPrior;
     private double artifactVsVariantPrior;
     private final OptionalDouble callableSites;
+
+    final List<Datum> data = new ArrayList<>();
 
     private final MutableDouble realVariantCount = new MutableDouble(0);
     private final MutableDouble realSNVCount = new MutableDouble(0);
@@ -44,15 +51,59 @@ public class SomaticPriorModel {
         technicalArtifactCount.add(errorProbabilities.getTechnicalArtifactProbability());
     }
 
-    public void recordDatum(final int[] tumorADs, final double tumorLog10Odds
+    public void recordDatum(final int[] tumorADs, final double[] tumorLog10Odds, final double artifactProbability) {
+        final int totalAD = (int) MathUtils.sum(tumorADs);
+        for (int i = 0; i < tumorLog10Odds.length; i++) {
+            data.add(new Datum(tumorLog10Odds[i], artifactProbability, tumorADs[i+1], totalAD));
+        }
+
+    }
 
     // by default, clear accumulated data after learning
     public void learnAndClearAccumulatedData() {
         learn();
+        data.clear();
         clear();
     }
 
     public void learn() {
+        Utils.resetRandomGenerator();
+        final RandomDataGenerator rng = Utils.getRandomDataGenerator()
+
+        final int noAssignment = -1;
+        final int artifactTable = 0;
+        final int totalPatrons = data.size();
+
+        // Chinese Restaurant tables:
+        // table 0: artifacts
+        // table 1: Beta background
+        // tables 2, 3 . . . single-AF tables
+
+        final List<Integer> tableAssignments = Collections.nCopies(data.size(), noAssignment);
+        final List<Pair<BetaDistributionShape, MutableInt>> betaShapesAndCounts = new ArrayList<>();
+        betaShapesAndCounts.add(ImmutablePair.of(null, new MutableInt(0)));
+
+        for (int iteration = 0; iteration < 5; iteration++) {
+            for (int n = 0; n < totalPatrons; n++) {
+                final Datum datum = data.get(n);
+                // stochastically assign to artifact
+                if (rng.nextUniform(0,1) < datum.artifactProb) {
+                    tableAssignments.set(n, artifactTable);
+                    betaShapesAndCounts.get(artifactTable).getRight().increment();
+                } else {
+                    final int table = 9;
+                    final double[] likelihoods
+
+                            tableAssignments.set(n, table);
+                    betaShapesAndCounts.get(table).getRight().increment();
+                }
+            }
+        }
+
+
+
+
+
         // global parameters
         artifactVsVariantPrior = (technicalArtifactCount.getValue() + 1) / (realVariantCount.getValue() + technicalArtifactCount.getValue() + 2);
         if (callableSites.isPresent()) {
@@ -73,5 +124,12 @@ public class SomaticPriorModel {
         final double artifactProb;
         final int altCount;
         final int totalCount;
+
+        public Datum(final double tumorLog10Odds, final double artifactProb, final int altCount, final int totalCount) {
+            this.tumorLog10Odds = tumorLog10Odds;
+            this.artifactProb = artifactProb;
+            this.altCount = altCount;
+            this.totalCount = totalCount;
+        }
     }
 }
